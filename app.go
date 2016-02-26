192ced390301fc43ec5a460e85e8f0eda2cc2d95
Steve Perkins <steve@steveperkins.com>
2016-02-26 15:23:49 -0500

    Adds endpoint for initializing empty JSON-Resume file

--- app.go
+++ app.go
@@ -2,17 +2,34 @@ package ResumeFodder
 
 import (
 	"appengine"
+	"gitlab.com/steve-perkins/ResumeFodder/data"
+	"html/template"
 	"net/http"
+	"path"
 )
 
 func init() {
-	http.Handle("/", http.FileServer(http.Dir("static")))
-	http.HandleFunc("/foo", defaultHandler)
+	http.HandleFunc("/test", testHandler)
+	http.HandleFunc("/init", initHandler)
 }
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	ctx := appengine.NewContext(r)
-	ctx.Infof("Hello world\n")
-	w.Header().Set("Content-Type", "text/html")
-	w.Write([]byte("Hello world"))
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles(path.Join("html", "test.html"))
+	if err != nil {
+		ctx := appengine.NewContext(r)
+		ctx.Errorf("Error loading 'test.html': %s\n", err)
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte("Unable to serve 'test.html'"))
+	}
+	t.Execute(w, nil)
+}
+
+func initHandler(w http.ResponseWriter, r *http.Request) {
+	json, err := data.ToJsonString(data.NewResumeData())
+	if err != nil {
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte(err.Error()))
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(json))
 }
