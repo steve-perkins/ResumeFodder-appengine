c4a9fc4da462512e34dd406b3d7e3807ed0abc4b
Steve Perkins <steve@steveperkins.com>
2016-02-26 18:01:30 -0500

    Adds support for uploading and parsing JSON-Resume data file

---
+++ README.md
@@ -0,0 +1,2 @@
+ResumeFodder-appengine
+======================
--- app.go
+++ app.go
@@ -2,6 +2,7 @@ package ResumeFodder
 
 import (
 	"appengine"
+	"fmt"
 	"gitlab.com/steve-perkins/ResumeFodder/data"
 	"html/template"
 	"net/http"
@@ -11,6 +12,7 @@ import (
 func init() {
 	http.HandleFunc("/test", testHandler)
 	http.HandleFunc("/init", initHandler)
+	http.HandleFunc("/generate", generateHandler)
 }
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +22,7 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
 		ctx.Errorf("Error loading 'test.html': %s\n", err)
 		w.Header().Set("Content-Type", "text/html")
 		w.Write([]byte("Unable to serve 'test.html'"))
+		return
 	}
 	t.Execute(w, nil)
 }
@@ -29,7 +32,41 @@ func initHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.Header().Set("Content-Type", "text/html")
 		w.Write([]byte(err.Error()))
+		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(json))
+}
+
+func generateHandler(w http.ResponseWriter, r *http.Request) {
+	// Update JSON-Resume data file
+	const MAX_UPLOAD_BYTES = 100000
+	file, _, _ := r.FormFile("file")
+	buffer := make([]byte, MAX_UPLOAD_BYTES)
+	bytesRead, _ := file.Read(buffer)
+	ctx := appengine.NewContext(r)
+	if bytesRead >= MAX_UPLOAD_BYTES {
+		ctx.Errorf("JSON-Resume upload exceeds the %d byte cap\n", MAX_UPLOAD_BYTES)
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte(fmt.Sprintf("Error: File uploads cannot exceed %d bytes\n", MAX_UPLOAD_BYTES)))
+		return
+	}
+	ctx.Infof("Uploaded %d bytes of JSON-Resume data\n", bytesRead)
+	contents := string(buffer[:bytesRead])
+
+	// Parse the (hopefully) JSON
+	_, err := data.FromJsonString(contents)
+	if err != nil {
+		ctx.Errorf("An error occurred parsing the uploaded file: %s\n", err)
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte("Cannot parse the uploaded file as JSON-Resume data\n"))
+		return
+	}
+
+	// Generate the resume
+	// TODO: Refactor the base ResumeFodder project, to provide an export method that works with in-memory strings rather than the filesystem
+
+	// TODO: Remove this filler
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(contents))
 }
