c62fa3974e621d4d278a720c91a65f1e9672e028
Steve Perkins <steve@steveperkins.com>
2016-02-28 21:46:55 -0500

    Implements 'generate.html' and 'error.html'

--- app.go
+++ app.go
@@ -14,28 +14,14 @@ import (
 
 func init() {
 	http.Handle("/", http.FileServer(http.Dir("static")))
-	http.HandleFunc("/test", testHandler)
 	http.HandleFunc("/init", initHandler)
 	http.HandleFunc("/generate", generateHandler)
-}
-
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles(path.Join("html", "test.html"))
-	if err != nil {
-		ctx := appengine.NewContext(r)
-		ctx.Errorf("Error loading 'test.html': %s\n", err)
-		w.Header().Set("Content-Type", "text/html")
-		w.Write([]byte("Unable to serve 'test.html'"))
-		return
-	}
-	t.Execute(w, nil)
 }
 
 func initHandler(w http.ResponseWriter, r *http.Request) {
 	json, err := command.InitResumeJson()
 	if err != nil {
-		w.Header().Set("Content-Type", "text/html")
-		w.Write([]byte(err.Error()))
+		errorHandler(w, r, err.Error())
 		return
 	}
 	w.Header().Set("Content-Type", "application/octet-stream")
@@ -44,16 +30,20 @@ func initHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func generateHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+
 	// Update JSON-Resume data file
 	const MAX_UPLOAD_BYTES = 100000
-	file, _, _ := r.FormFile("file")
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		errorHandler(w, r, fmt.Sprintf("An error occurred uploading the file: %s\n", err))
+		return
+	}
+	ctx.Infof("file == %s\n", file)
 	uploadBuffer := make([]byte, MAX_UPLOAD_BYTES)
 	bytesRead, _ := file.Read(uploadBuffer)
-	ctx := appengine.NewContext(r)
 	if bytesRead >= MAX_UPLOAD_BYTES {
-		ctx.Errorf("JSON-Resume upload exceeds the %d byte cap\n", MAX_UPLOAD_BYTES)
-		w.Header().Set("Content-Type", "text/html")
-		w.Write([]byte(fmt.Sprintf("Error: File uploads cannot exceed %d bytes\n", MAX_UPLOAD_BYTES)))
+		errorHandler(w, r, fmt.Sprintf("JSON Resume upload exceeds the %d byte cap\n", MAX_UPLOAD_BYTES))
 		return
 	}
 	ctx.Infof("Uploaded %d bytes of JSON-Resume data\n", bytesRead)
@@ -62,26 +52,20 @@ func generateHandler(w http.ResponseWriter, r *http.Request) {
 	// Parse the (hopefully) JSON
 	resumeData, err := data.FromJsonString(contents)
 	if err != nil {
-		ctx.Errorf("An error occurred parsing the uploaded file: %s\n", err)
-		w.Header().Set("Content-Type", "text/html")
-		w.Write([]byte("Cannot parse the uploaded file as JSON-Resume data\n"))
+		errorHandler(w, r, fmt.Sprintf("An error occurred parsing the uploaded file: %s\n", err))
 		return
 	}
 
 	// Load the selected template
 	templateParam := r.Form.Get("template")
-	if templateParam != "plain" && templateParam != "iconic" && templateParam != "refined" {
-		ctx.Errorf("An unrecognized template was selected: %s\n", templateParam)
-		w.Header().Set("Content-Type", "text/html")
-		w.Write([]byte("Cannot load the template\n"))
+	if templateParam != "standard" && templateParam != "iconic" && templateParam != "refined" {
+		errorHandler(w, r, fmt.Sprintf("An unrecognized template was selected: %s\n", templateParam))
 		return
 	}
 	ctx.Infof("Exporting a resume with template: %s\n", templateParam)
 	templateBytes, err := ioutil.ReadFile(filepath.Join("templates", templateParam+".xml"))
 	if err != nil {
-		ctx.Errorf("An error occurred loading the template file: %s\n", err)
-		w.Header().Set("Content-Type", "text/html")
-		w.Write([]byte("Cannot load the template\n"))
+		errorHandler(w, r, fmt.Sprintf("An error occurred loading the template file: %s\n", err))
 		return
 	}
 	templateString := string(templateBytes)
@@ -89,12 +73,27 @@ func generateHandler(w http.ResponseWriter, r *http.Request) {
 	// Generate the resume
 	exportBuffer, err := command.ExportResume(resumeData, templateString)
 	if err != nil {
-		ctx.Errorf("An error occurred exporting the resume: %s\n", err)
-		w.Header().Set("Content-Type", "text/html")
-		w.Write([]byte("An error occurred exporting the resume\n"))
+		errorHandler(w, r, fmt.Sprintf("An error occurred exporting the resume: %s\n", err))
 		return
 	}
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Header().Set("Content-Disposition", "attachment;filename=\"resume.doc\"")
 	w.Write(exportBuffer.Bytes())
 }
+
+func errorHandler(w http.ResponseWriter, r *http.Request, message string) {
+	ctx := appengine.NewContext(r)
+	ctx.Errorf("Handling error: %s\n", message)
+
+	w.Header().Set("Content-Type", "text/html")
+	t, err := template.ParseFiles(path.Join("static", "error.html"))
+	if err != nil {
+		ctx.Errorf("Error loading 'error.html': %s\n", err)
+		w.Write([]byte("Unable to render the error page"))
+		return
+	}
+	data := make(map[string]string)
+	data["message"] = message
+	t.Execute(w, data)
+}
+
